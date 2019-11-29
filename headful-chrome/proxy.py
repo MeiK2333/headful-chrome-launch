9726cb6a2c37243280b4169a2b4852d4793dc71f
MeiK <meik2333@gmail.com>
2019-11-29 19:26:54 +0800

    Update headful-chrome

---
+++ headful-chrome/proxy.py
@@ -0,0 +1,14 @@
+from mitmproxy import http
+
+
+def request(flow: http.HTTPFlow) -> None:
+    if flow.request.method == "CONNECT":
+        return
+    if flow.live:
+        proxy = flow.request.headers.get("mitmproxy")
+        if not proxy:
+            return
+        print(f"use proxy {proxy}")
+        del flow.request.headers["mitmproxy"]
+        host, port = proxy.split(":")
+        flow.live.change_upstream_proxy_server((host, int(port)))
