405ca749cbe32b344a928e9e2f58b126dbe79f3a
MeiK <meik2333@gmail.com>
2020-03-09 19:40:15 +0800

    add try catch

--- README.md
+++ README.md
@@ -3,8 +3,15 @@
 ## Run
 
 ```
-docker build . -t headful-crawler
-docker run --rm -it -p 5678:5678 headful-crawler
+docker pull meik2333/headful-chrome-launch
+docker run --rm -it -p 5678:5678 headful-chrome-launch
+```
+
+or
+
+```
+docker build . -t headful-chrome-launch
+docker run --rm -it -p 5678:5678 headful-chrome-launch
 ```
 
 or
--- app.ts
+++ app.ts
@@ -53,39 +53,40 @@ var proxyServer = http.createServer(function (req, res) {
 });
 
 proxyServer.on('upgrade', async (req, socket, head) => {
-  const browserType = req.url.split('/')[1].toLowerCase();
-  var browser;
-  switch (browserType) {
-    case 'chrome':
-      browser = await playwright.chromium.launchServer({
-        executablePath: '/usr/bin/google-chrome',
-        headless: false,
-        args: [
-          '--disable-dev-shm-usage',
-          '--disable-setuid-sandbox',
-          '--no-sandbox',
-          '--proxy-server=http://127.0.0.1:8080',
-          '--no-first-run',
-          '--no-default-browser-check'
-        ]
-      });
-      break;
-    case 'chromium':
-      browser = await playwright.chromium.launchServer({
-        headless: false,
-        args: [
-          '--disable-dev-shm-usage',
-          '--disable-setuid-sandbox',
-          '--no-sandbox',
-          '--proxy-server=http://127.0.0.1:8080',
-          '--no-first-run',
-          '--no-default-browser-check'
-        ]
-      });
-      break;
-    case 'firefox':
-      // 将代理配置写入 Firefox 的配置文件中，并以此配置文件启动
-      const firefoxUserJs = `
+  try {
+    const browserType = req.url.split('/')[1].toLowerCase();
+    var browser;
+    switch (browserType) {
+      case 'chrome':
+        browser = await playwright.chromium.launchServer({
+          executablePath: '/usr/bin/google-chrome',
+          headless: false,
+          args: [
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--no-sandbox',
+            '--proxy-server=http://127.0.0.1:8080',
+            '--no-first-run',
+            '--no-default-browser-check'
+          ]
+        });
+        break;
+      case 'chromium':
+        browser = await playwright.chromium.launchServer({
+          headless: false,
+          args: [
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--no-sandbox',
+            '--proxy-server=http://127.0.0.1:8080',
+            '--no-first-run',
+            '--no-default-browser-check'
+          ]
+        });
+        break;
+      case 'firefox':
+        // 将代理配置写入 Firefox 的配置文件中，并以此配置文件启动
+        const firefoxUserJs = `
 user_pref("security.tls.version.min", 1);
 user_pref("network.stricttransportsecurity.preloadlist", false);
 user_pref("network.proxy.type", 1);
@@ -95,36 +96,40 @@ user_pref("network.proxy.http_port", 8080);
 user_pref("network.proxy.ssl", "127.0.0.1");
 user_pref("network.proxy.ssl_port", 8080);
       `;
-      const userDataDir = await mkdtempAsync(path.join(os.tmpdir(), 'playwright_dev_firefox_profile-'));
-      await writeFileAsync(path.join(userDataDir, "./user.js"), firefoxUserJs);
-      browser = (await playwright.firefox._launchServer({
-        headless: false
-      }, 'server', userDataDir)).browserServer;
-      break;
-    case 'webkit':
-      // TODO: 暂时无法启动
-      browser = await playwright.webkit.launchServer({
-        headless: false
-      });
-      break;
-    default:
-      console.log(`Unknown browser: ${browserType}`);
-      socket.end();
-      return;
+        const userDataDir = await mkdtempAsync(path.join(os.tmpdir(), 'playwright_dev_firefox_profile-'));
+        await writeFileAsync(path.join(userDataDir, "./user.js"), firefoxUserJs);
+        browser = (await playwright.firefox._launchServer({
+          headless: false
+        }, 'server', userDataDir)).browserServer;
+        break;
+      case 'webkit':
+        // TODO: 暂时无法启动
+        browser = await playwright.webkit.launchServer({
+          headless: false
+        });
+        break;
+      default:
+        console.log(`Unknown browser: ${browserType}`);
+        socket.end();
+        return;
+    }
+    console.log(`${browserType}: ${browser.wsEndpoint()}`);
+    socket.on('close', async () => {
+      await browser.close();
+      console.log(`${browserType}: ${browser.wsEndpoint()} closed`);
+    });
+    socket.on('error', async () => {
+      await browser.close();
+      console.log(`${browserType}: ${browser.wsEndpoint()} error`);
+    });
+    proxy.ws(req, socket, head, {
+      target: browser.wsEndpoint(),
+      ignorePath: true
+    });
+  } catch (err) {
+    console.error(err);
+    socket.end();
   }
-  console.log(`${browserType}: ${browser.wsEndpoint()}`);
-  socket.on('close', async () => {
-    await browser.close();
-    console.log(`${browserType}: ${browser.wsEndpoint()} closed`);
-  });
-  socket.on('error', async () => {
-    await browser.close();
-    console.log(`${browserType}: ${browser.wsEndpoint()} error`);
-  });
-  proxy.ws(req, socket, head, {
-    target: browser.wsEndpoint(),
-    ignorePath: true
-  });
 });
 
 proxyServer.listen(5678);
