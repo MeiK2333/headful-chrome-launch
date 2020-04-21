9c2b81f925050ed3e458f91e884f8c1e011b5c07
MeiK <meik2333@gmail.com>
2020-04-21 14:44:08 +0800

    删除无关配置文件

--- src/extensions.ts
+++ src/extensions.ts
@@ -1,5 +1,3 @@
-import * as playwright from 'playwright';
-
 export const extensions = [
   './extensions/chromium/fhkphphbadjkepgfljndicmgdlndmoke/0.1.1_0',
   './extensions/chromium/fjkmabmdepjfammlpliljpnbhleegehm/0.2.6_0',
@@ -8,27 +6,3 @@ export const extensions = [
   './extensions/chromium/pcbjiidheaempljdefbdplebgdgpjcbe/0.1.4_0',
   './extensions/chromium/pkehgijcmpdhfbdbbnkijodmdjhbjlgp/2020.2.19_0',
 ];
-
-export async function chromiumUseExtension(browserContext: playwright.BrowserContext) {
-  const page = await browserContext.newPage();
-  await page.goto('chrome://extensions');
-  for (let i = 0; i < 5; i++) {
-    if (browserContext.pages().length > 2) {
-      await browserContext.pages()[2].close();
-      break;
-    }
-    if (i === 4) {
-      throw new Error('扩展应用失败');
-    }
-    await page.waitFor(300 * (i + 1));
-  }
-  for (let i = 0; i < extensions.length; i++) {
-    await page.goto('chrome://extensions');
-    await page.waitFor(200);
-    await page.click(`css=body > extensions-manager >> css=#items-list >> css=#${extensions[i].split('/')[3]} >> css=#detailsButton`);
-    await page.waitFor(200);
-    await page.click('css=body > extensions-manager >> css=#viewManager > extensions-detail-view >> css=#allow-incognito');
-    await page.waitFor(200);
-  }
-  await page.waitFor(1000);
-};
