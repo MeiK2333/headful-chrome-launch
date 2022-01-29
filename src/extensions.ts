34f71cf7dd1166188b5fbb6ecc366146b6c8db4b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-29 12:06:45 +0800

    Delete src directory

--- src/extensions.ts
+++
@@ -1,8 +0,0 @@
-export const extensions = {
-  'fhkphphbadjkepgfljndicmgdlndmoke': './extensions/chromium/fhkphphbadjkepgfljndicmgdlndmoke/0.1.1_0',
-  'fjkmabmdepjfammlpliljpnbhleegehm': './extensions/chromium/fjkmabmdepjfammlpliljpnbhleegehm/0.2.6_0',
-  'lanfdkkpgfjfdikkncbnojekcppdebfp': './extensions/chromium/lanfdkkpgfjfdikkncbnojekcppdebfp/0.1.6_0',
-  'olnbjpaejebpnokblkepbphhembdicik': './extensions/chromium/olnbjpaejebpnokblkepbphhembdicik/0.1.3_0',
-  'pcbjiidheaempljdefbdplebgdgpjcbe': './extensions/chromium/pcbjiidheaempljdefbdplebgdgpjcbe/0.1.4_0',
-  'pkehgijcmpdhfbdbbnkijodmdjhbjlgp': './extensions/chromium/pkehgijcmpdhfbdbbnkijodmdjhbjlgp/2020.2.19_0',
-};
